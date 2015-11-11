5aa5c86da09b08d60d48221fd78827869f9314e9
llsposto <llsposto@usc.edu>
2015-11-10 23:10:34 -0800

    Writeup and stuff

---
+++ code/test/testcondition.c
@@ -0,0 +1,15 @@
+#include "syscall.h"
+
+int main() {
+	int c = -1;
+	int l = -1;
+	l = CreateLock("Lock 1", 6);
+	Acquire(l);
+
+	c = CreateCondition("Condition 1",11);
+	Broadcast(c, l);
+
+	Release(l);
+	DestroyCondition(c);
+	DestroyLock(l);
+}
---
+++ code/test/testmonitorbad.c
@@ -0,0 +1,11 @@
+#include "syscall.h"
+
+int main() {
+    int c = -1;
+    int l = -1;
+    int m = -1;
+
+    m = CreateMonitor(l, c, 1);
+
+    GetMonitor(-1);
+}
